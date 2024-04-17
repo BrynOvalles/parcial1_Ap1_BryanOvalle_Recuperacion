073e8aaee63a0cbb94988ee110661f483c78bb0d
BrynOvalles <brynovallesburgos@gmail.com>
2024-04-16 22:06:41 -0400

    Creando el modelo de metas

---
+++ parcial1_Ap1_BryanOvalle_Recuperacion/Models/Metas.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace parcial1_Ap1_BryanOvalle_Recuperacion.Models;
+
+public class Metas
+{
+	[Key]
+	public int MetaId { get; set; }
+	[Required(ErrorMessage = "Es requerido intruducir una fecha")]
+	[DataType(DataType.Date)]
+	[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+	public DateTime Fecha { get; set; } = DateTime.Now;
+	[Required(ErrorMessage = "Es requerido que tenga una descripción")]
+	[MinLength(3, ErrorMessage = "La descripción es muy corta")]
+	[MaxLength(100, ErrorMessage = "La descripción es muy larga")]
+	public string? Descripcion { get; set; }
+	[Required(ErrorMessage = "Es requerido que ingreses un monto para tu meta")]
+	[Range(minimum: 0.01, maximum: 10000000000000000000, ErrorMessage = "El monto debe ser mayor a 0 o es demasiado alto para el sistema")]
+	public decimal Monto { get; set; }
+}
